e9b4cf044a8fafd1d3e0b93efbc35ca44ec28052
Pablo Alecio <paleciop@gmail.com>
2017-08-27 21:36:08 -0500

    First implementation of the templating engine.

--- examples/example.js
+++ examples/example.js
@@ -4,28 +4,28 @@ console.log('>>>> Testing Nova Core.');
 const nova = require('../index');
 const fsPromised = require('../lib/fsPromised');
 
+const templatingEngine = nova.templatingEngine;
+
 async function test () {
   const cpe = await nova.fetchContextProcessorEngine({paths: ['examples/contextprocessors']});
-  cpe.execute('test', {}).then(contentModel => console.log('Content Model: ', contentModel));
+  cpe.execute('test', {}).then(contentModel => {
+    console.log('Content Model: ', contentModel);
+    const html = `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>Title</title>
+        </head>
+        <body>
+            <p>Test: {% test %}</p>
+            <p>Test2: {% test2 %}</p>
+            <p>Test3: {%test3%}</p>
+            <p>Test5: {%test5%}</p>
+        </body>
+        </html>`;
+    console.log(templatingEngine.compileTemplate(html, contentModel))
+  });
 
 }
 test();
 
-/*
-fsPromised.walkFiles('./test/contextprocessors').then(files => console.log(files));
-*/
-
-/*const contextProcessor = require('../lib/contextProcessor');
-
-console.log(contextProcessor);*/
-
-/*
-async function test () {
-  console.log('Before');
-  const a = await Promise.resolve('Async Await Test');
-  console.log('After');
-  return a;
-}
-
-test().then(a => console.log('Test results:', a));
-*/
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const fetchContextProcessorEngine = require('./lib/fetchContextProcessorEngine');
+const templatingEngine = require('./lib/templatingEngine');
 const contextProcessor = require('./lib/contextProcessor');
 
 module.exports = {
   fetchContextProcessorEngine,
+  templatingEngine,
   contextProcessor
 };
