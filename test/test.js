7020207151aba12d85542fc745c8054595c3b778
Pablo Alecio <paleciop@gmail.com>
2017-08-27 19:50:17 -0500

    Added Jest.

--- test/test.js
+++ test/test.js
@@ -1,31 +1,8 @@
-'use strict';
-console.log('>>>> Testing Nova Core.');
 
-const nova = require('../index');
-const fsPromised = require('../lib/fsPromised');
-
-async function test () {
-  const cpe = await nova.fetchContextProcessorEngine({paths: ['test/contextprocessors']});
-  cpe.execute('test', {}).then(contentModel => console.log('Content Model: ', contentModel));
-
-}
-test();
-
-/*
-fsPromised.walkFiles('./test/contextprocessors').then(files => console.log(files));
-*/
-
-/*const contextProcessor = require('../lib/contextProcessor');
-
-console.log(contextProcessor);*/
-
-/*
-async function test () {
-  console.log('Before');
-  const a = await Promise.resolve('Async Await Test');
-  console.log('After');
-  return a;
+function sum(a, b) {
+  return a + b;
 }
 
-test().then(a => console.log('Test results:', a));
-*/
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
