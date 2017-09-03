9b82e1fea20e0cacf265d1e694b2ca90c2c318aa
Pablo Alecio <paleciop@gmail.com>
2017-09-03 02:28:59 -0500

    Refactored how the matches worked. Refactored how the executionContext was being sent to the process methods.

--- examples/contextprocessors/otherPathCP.js
+++ examples/contextprocessors/otherPathCP.js
@@ -3,10 +3,10 @@
 const contextProcessor = require('../../index').pathAwareContextProcessor;
 
 module.exports = contextProcessor.extend({
-  patterns: ["/api/document/*", "/api/docs/:name"],
-  name: 'Path Context Processor',
+  patterns: ["/api/document/*", "/api/docs/testname"],
+  name: 'Other Path Context Processor',
   process (executionContext, contentModel) {
     console.log('Execution Context:', executionContext);
-    contentModel.test = 'Path CP Property';
+    contentModel.test2 = 'Path CP Property2';
   }
 });
